54ae89c7c4f1b1cb196ac7899e6e8a9983159cdc
sixdgt <c4crypt@gmail.com>
2022-12-15 09:40:43 +0545

    items list, show & delete

--- project_inventory/app_inventory/urls.py
+++ project_inventory/app_inventory/urls.py
@@ -3,9 +3,10 @@ from . import views
 urlpatterns = [
     # items
     path("items/", views.item_index, name="items.index"),
-    path("items/edit/", views.item_edit, name="items.edit"),
+    path("items/edit/<int:id>/", views.item_edit, name="items.edit"),
     path("items/create/", views.item_create, name="items.create"),
-    path("items/show/", views.item_show, name="items.show"),
+    path("items/show/<int:id>/", views.item_show, name="items.show"),
+    path("items/delete/<int:id>/", views.item_delete, name="items.delete"),
 
     #  users
     path("users/login/", views.user_login, name="users.login"),
--- project_inventory/app_inventory/views.py
+++ project_inventory/app_inventory/views.py
@@ -1,33 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ItemCreateForm, UserLoginForm, UserRegisterForm
 from datetime import datetime
 from .models import Item, AppUser
 
 # Create your views here.
 def item_index(request):
-    return render(request, "items/index.html")
+    item_list = Item.objects.all()
+    context = {"item_list": item_list}
+    return render(request, "items/index.html", context)
 
-def item_show(request):
-    return render(request, "items/show.html")
+def item_show(request, id):
+    data = Item.objects.get(id=id)
+    context = {"data": data}
+    return render(request, "items/show.html", context)
 
-def item_edit(request):
-    return render(request, "items/edit.html")
+def item_edit(request, id):
+    data = Item.objects.get(id=id)
+    context = {"data": data}
+    return render(request, "items/edit.html", context)
+
+def item_delete(request, id):
+    data = Item.objects.get(id=id)
+    data.delete()
+    return redirect("items.index")
 
 def item_create(request):
     form = ItemCreateForm()
     context = {"form": form}
     if request.method == "POST":
-        item = Item()
+        item_obj = Item()
         user = AppUser.objects.get(id=1)
-        item.title = request.POST.get("title")
-        item.particular = request.POST.get("particular")
-        item.lf = request.POST.get("lf")
-        item.price = request.POST.get("price")
-        item.quantity = request.POST.get("quantity")
-        item.total = request.POST.get("total")
-        item.added_at = datetime.now()
-        item.user = user
-        item.save()
+        item_obj.title = request.POST.get("title")
+        item_obj.particular = request.POST.get("particular")
+        item_obj.lf = request.POST.get("lf")
+        item_obj.price = request.POST.get("price")
+        item_obj.quantity = request.POST.get("quantity")
+        item_obj.total = request.POST.get("total")
+        item_obj.added_at = datetime.now()
+        item_obj.user = user
+        item_obj.save()
         context.setdefault("msg", "Item Created Successfully")
         # item = Item(title=title, particular=particular)
     return render(request, "items/create.html", context)
